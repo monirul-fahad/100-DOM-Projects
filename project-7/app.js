86e53291ed1f09a65232755c2d502c61afddbd90
monirul-fahad <immonirulfahad@gmail.com>
2022-05-11 08:07:55 +0600

    project-7

--- project-7/app.js
+++ project-7/app.js
@@ -1,31 +1,100 @@
+let div = null;
+
 window.onload = () => {
 	main();
 };
 
+
 function main() {
 	const root = document.getElementById('root');
 	const output = document.getElementById('output');
+	const output2 = document.getElementById('output2');
 	const changeBtn = document.getElementById('change-btn');
 	const copyBtn = document.getElementById('copy-btn');
 
 	changeBtn.addEventListener('click', function () {
-		const bgColor = generateHexColor();
-		root.style.backgroundColor = bgColor;
-		output.value = bgColor;
+		const color = generateColorDecimal();
+		const hex = generateHexColor(color);
+		const rgb = generateRGBColor(color);
+		root.style.backgroundColor = hex;
+		output.value = hex.substring(1);
+		output2.value = rgb;
 	});
 
 	copyBtn.addEventListener('click', function () {
-		navigator.clipboard.writeText(output.value);
+		navigator.clipboard.writeText(`#${output.value}`);
+		if (div !== null) {
+			div.remove();
+			div = null;
+		}
+		if (isValidHex(output.value)) {
+			generateToastMessage(`#${output.value} copied`);
+		} else {
+			alert('Invalid Color Code');
+		}
+	});
+
+	output.addEventListener('keyup', function (e) {
+		const color = e.target.value;
+		if (color) {
+			output.value = color.toUpperCase();
+			if (isValidHex(color)) {
+				root.style.backgroundColor = `#${color}`;
+			}
+		}
 	});
 }
 
-// step 2 - random color generator function
-function generateHexColor() {
-	// #000000 #ffffff
-	// 255, 255, 255 -> #FFFFFF
+
+function generateColorDecimal() {
 	const red = Math.floor(Math.random() * 255);
 	const green = Math.floor(Math.random() * 255);
 	const blue = Math.floor(Math.random() * 255);
 
-	return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+	return {
+		red,
+		green,
+		blue,
+	};
+}
+
+function generateHexColor({ red, green, blue }) {
+	const getTwoCode = (value) => {
+		const hex = value.toString(16);
+		return hex.length === 1 ? `0${hex}` : hex;
+	};
+
+	return `#${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(
+		blue
+	)}`.toUpperCase();
+}
+
+function generateRGBColor({ red, green, blue }) {
+	return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function generateToastMessage(msg) {
+	div = document.createElement('div');
+	div.innerText = msg;
+	div.className = 'toast-message toast-message-slide-in';
+
+	div.addEventListener('click', function () {
+		div.classList.remove('toast-message-slide-in');
+		div.classList.add('toast-message-slide-out');
+
+		div.addEventListener('animationend', function () {
+			div.remove();
+			div = null;
+		});
+	});
+
+	document.body.appendChild(div);
+}
+
+/**
+ * @param {string} color : ;
+ */
+function isValidHex(color) {
+	if (color.length !== 6) return false;
+	return /^[0-9A-Fa-f]{6}$/i.test(color);
 }
